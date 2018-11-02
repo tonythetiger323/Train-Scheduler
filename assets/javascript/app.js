c98aae2c707e69533cfba2b3e986955d7e40dbd6
Tony Greeley <tony.greeley@me.com>
2018-11-02 03:37:04 -0500

    grrr

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,35 +18,30 @@ trainRef.on("child_added", function (childSnapshot) {
     //stores cuurent time into a variable
     var presentTime = moment();
     //converts train times from military time to 12 hour format and stores it to a variable
-    let convertTime = moment(firstTrainTime, "hh:mm").subtract(1, "years");
-    console.log("comvertTime", convertTime);
-    let diffTime = presentTime.diff(moment(convertTime), "minutes");
-    console.log("diffTime", diffTime);
-    //get the remainder of dividng time between trains in minutes divided by the train frequency which is the first variable needed to determine homw many minutes until the next train arrives
-    let remainder = (diffTime % childSnapshotData.frequency);
-    console.log("remainder", remainder);
-    let minNext = (childSnapshotData.frequency - remainder);
-    console.log("minNext", minNext);
+    var momentFirstTrain = moment(childSnapshotData.firstTrainTime, "HH:mm").subtract(1, "years");
 
+    //find out the number of minutes between now and when the first train came and stores it to a variable
+    var timeBetweenTrains = presentTime.diff(moment(momentFirstTrain), "minutes");
 
-    let nextTrain = moment().add(minNext, "minutes");
-    console.log("nextTrain", nextTrain)
-    let formatNextTrain = moment(nextTrain).format("hh:mm");
-    console.log("formatNextTrain", formatNextTrain);
-
+    //get the remainder of diving time between trains in minutes divided by the train frequency which is the first variable needed to determine homw many minutes until the next train arrives
+    var minutesRemain = timeBetweenTrains % childSnapshotData.frequency;
 
     //find out how many minutes until the next train arrives and stores it to a variable
-
+    var minNextTrain = childSnapshotData.frequency - minutesRemain;
 
     //get the time the next train arrives by taking the current time and adding how many minutes til next train to it
+    var nextTrainTime = presentTime.add(minNextTrain, "minutes");
+
+    //format time back 12 hr
+    var timeAmPm = moment(nextTrainTime).format("hh:mm");
 
 
     var newRow = $("<tr>").append(
         $("<td>").text(childSnapshotData.trainName),
         $("<td>").text(childSnapshotData.destination),
         $("<td>").text(childSnapshotData.frequency),
-        $("<td>").text(formatNextTrain),
-        $("<td>").text(minNext)
+        $("<td>").text(timeAmPm),
+        $("<td>").text(minNextTrain)
     );
 
     $("#train-table > tbody").append(newRow);
